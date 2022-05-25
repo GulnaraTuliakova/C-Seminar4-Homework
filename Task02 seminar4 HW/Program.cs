b85179431ff980d77ad14681af9f8b5f72f7a594
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-25 22:30:31 +0500

    Add task02 seminar4 HW

---
+++ Task02 seminar4 HW/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задать массив из 12 элементов, заполненных числами из [-9,9]. Найти сумму положительных/отрицательных элементов массива
+Console.WriteLine("Зададим массив из 12 элементов, заполненных числами [-9,9], выведем его на экран");
+int [] array = new int [12];
+for (int i = 0; i < 12; i++)
+{
+    array [i] = new Random().Next(-9,10);
+    Console.Write($"{array [i]}  ");
+}
+int sumMin = 0;
+int sumMax = 0;
+for (int i = 0; i < 12; i++)
+{
+    if (array [i] < 0) sumMin = sumMin + array [i];
+    if (array [i] > 0) sumMax = sumMax + array [i];
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма отрицательных элементов массива = {sumMin}");
+Console.WriteLine($"Сумма отрицательных элементов массива = {sumMax}");
