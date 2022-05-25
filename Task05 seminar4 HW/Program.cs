bd17854c40bca988bad527ccda509736b61d4ead
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-05-25 23:12:12 +0500

    Add task05 seminar4 HW

---
+++ Task05 seminar4 HW/Program.cs
@@ -0,0 +1,16 @@
+﻿//Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+Console.WriteLine("Найдем произведение пар чисел в одномерном массиве. Введите количество чисел в массиве:");
+int num = int.Parse(Console.ReadLine()?? "");
+int [] array = new int [num];
+for (int i = 0; i < num; i++)
+{
+    array [i] = new Random().Next(-100,100);
+    Console.WriteLine($"{array [i]}  ");
+}
+int prod = -1;
+
+for (int i = 0; i < num/2; i++)
+{
+    prod = array [i] * array [num-1-i];
+    Console.WriteLine($"{array [i]} * {array [num-1-i]} = {prod}");
+}
